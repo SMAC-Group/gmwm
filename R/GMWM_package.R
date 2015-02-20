36224a2f6b4b7a056cb060347710f1fa5dfe5352
coatless <balamut2@illinois.edu>
2015-02-19 20:42:25 -0600

    Package updates...
    Former-commit-id: 951a8c4bca8f20a7fa45254f15b96f99c869a849

---
+++ R/GMWM_package.R
@@ -0,0 +1,27 @@
+#' GMWM Package
+#'
+#' GMWM is a Time Series Package
+#' 
+#' @details 
+#' \tabular{ll}{
+#' Package: \tab GMWM\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.6.2\cr
+#' Date: \tab 2015-02-17\cr
+#' License: \tab MIT + file\cr
+#' }
+#' Some details should be added  
+#' 
+#' @author
+#' Stephane Guerrier \email{stephane@@illinois.edu},
+#' James Balamuta \email{balamut2@@illinois.edu}
+#'
+#' James Balamuta \email{balamut2@@illinois.edu}
+#' @name GMWM-package
+#' @docType package
+#' @useDynLib GMWM
+#' @importFrom Rcpp sourceCpp
+#' @examples
+#' # Fill in at a later time
+#' @seealso \code{avar}
+NULL
