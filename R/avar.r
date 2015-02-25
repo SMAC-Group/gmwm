13a04c0613e04334fef884b0957e67c90021d43b
coatless <balamut2@illinois.edu>
2015-02-24 22:34:25 -0600

    Added documentation.
    Changed S3 objects to fit generic

--- R/avar.r
+++ R/avar.r
@@ -1,7 +1,18 @@
 ## Creates RcppArmadillo S3 object export within R
-avar = function(x, ...) UseMethod("avar")
 
-avar.default = function(x, ...) {
+
+
+#' @title Calculate the Allan Variance
+#' @description Computes the Allan Variance
+#' @usage avar(x)
+#' @param x A \code{vec} containing the time series under observation.
+#' @return Allan variance fixed
+#' @author JJB
+#' @examples
+#' set.seed(999)
+#' x=rnorm(100)
+#' avar(x)
+avar = function(x) {
   x = as.vector(x)
   av = .Call('GMWM_avar_fixed_arma', PACKAGE = 'GMWM', x)
   av$adev = sqrt(av$allan)
@@ -11,6 +22,18 @@ avar.default = function(x, ...) {
   av
 }
 
+#' @title Prints Allan Variance
+#' @description Displays the allan variance information
+#' @method print avar
+#' @param x A \code{avar} object.
+#' @param ... Arguments to be passed to methods
+#' @author JJB
+#' @return console output
+#' @examples
+#' set.seed(999)
+#' x=rnorm(100)
+#' out = avar(x)
+#' print( out )
 print.avar = function(x, ...) {
   cat("\n Clusters: \n")
   print(x$clusters, digits=5)
@@ -20,6 +43,18 @@ print.avar = function(x, ...) {
   print(x$errors, digits=5)
 }
 
+#' @title Plot Allan Variance
+#' @description Displays a plot containing the allan variance
+#' @method plot avar
+#' @param x A \code{avar} object.
+#' @param ... Arguments to be passed to methods
+#' @author JJB
+#' @return ggplot2 graph
+#' @examples
+#' set.seed(999)
+#' x=rnorm(100)
+#' out = avar(x)
+#' plot( out )
 plot.avar = function(x, ...){
   plot(x$clusters, x$adev,log="xy",
        xlab=expression("Scale " ~ tau),
@@ -31,21 +66,29 @@ plot.avar = function(x, ...){
   lines(x$clusters, x$uci, type="l", col="grey", lty=2)
 }
 
-summary.avar = function(x, ...) {
-  out_matrix = matrix(0, nrow = length(x$clusters), ncol = 6)
-  colnames(out_matrix) = c("Time", "AVAR", "ADEV", "Lower CI", "Upper CI", "Error")
-  out_matrix[,"Time"] = x$clusters
-  out_matrix[,"AVAR"] = x$allan
-  out_matrix[,"ADEV"] = x$adev
-  out_matrix[,"Lower CI"] = x$lci
-  out_matrix[,"Upper CI"] = x$uci
-  out_matrix[,"Error"] = x$errors
-  
-  class(x) = "summary.avar"
-  out_matrix
-}
 
-print.summary.avar = function(x, ...) {
-  print(x)
-  invisible(x)
+#' @title Summary Allan Variance
+#' @description Displays the summary table of allan variance
+#' @method summary avar
+#' @param object A \code{avar} object.
+#' @param ...  additional arguments affecting the summary produced.
+#' @author JJB
+#' @return Summary table
+#' @examples
+#' set.seed(999)
+#' x=rnorm(100)
+#' out = avar(x)
+#' summary( out )
+summary.avar = function(object, ...) {
+  out_matrix = matrix(0, nrow = length(object$clusters), ncol = 6)
+  colnames(out_matrix) = c("Time", "AVAR", "ADEV", "Lower CI", "Upper CI", "Error")
+  out_matrix[,"Time"] = object$clusters
+  out_matrix[,"AVAR"] = object$allan
+  out_matrix[,"ADEV"] = object$adev
+  out_matrix[,"Lower CI"] = object$lci
+  out_matrix[,"Upper CI"] = object$uci
+  out_matrix[,"Error"] = object$errors
+  
+  class(out_matrix) = "summary.avar"
+  out_matrix
 }
