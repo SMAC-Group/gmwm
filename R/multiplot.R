38c9274ba40f36e69573a6a6a68d2be66c6d43f8
coatless <balamut2@illinois.edu>
2015-02-24 22:35:40 -0600

    Removed grid call

--- R/multiplot.R
+++ R/multiplot.R
@@ -13,11 +13,13 @@
 #' 
 #' See the R Graphics Cookbook for more information
 #' @author Winston Chang 
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  require(grid)
+multiplot = function(..., plotlist=NULL, cols=1, layout=NULL) {
+  #require(grid)
+  
+  grid.newpage=pushViewport=viewport=grid.layout=NULL
   
   # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
+  plots = c(list(...), plotlist)
   
   numPlots = length(plots)
   
@@ -26,7 +28,7 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     # Make the panel
     # ncol: Number of columns of plots
     # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+    layout = matrix(seq(1, cols * ceiling(numPlots/cols)),
                      ncol = cols, nrow = ceiling(numPlots/cols))
   }
   
@@ -41,7 +43,7 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     # Make each plot, in the correct location
     for (i in 1:numPlots) {
       # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      matchidx = as.data.frame(which(layout == i, arr.ind = TRUE))
       
       print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
                                       layout.pos.col = matchidx$col))
