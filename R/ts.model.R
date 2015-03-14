b2eafffa07a26c3a992c65fd26a2e8746c12841b
coatless <james.balamuta@gmail.com>
2015-03-14 02:01:39 -0500

    Updated the time series model class to include:  'plength', 'obj.desc', 'obj', and 'adv'
    Changed default constructors to use NULL. If null is detected at all, force the model into a guided mode.
    
    
    Former-commit-id: 3f281567869b1705f2c79cb2300419758c351ed9

--- R/ts.model.R
+++ R/ts.model.R
@@ -16,151 +16,193 @@ is.whole = function(x){ is.numeric(x) && floor(x)==x }
 #' @description Setups the necessary backend for the AR1 process.
 #' @usage AR1(phi = 0.91, sigma = 1)
 #' @param phi A \code{double} value for the \eqn{\phi}{phi} of an AR1 process.
-#' @param sigma A \code{double} value for the \eqn{\sigma}{sigma} process.
+#' @param sigma2 A \code{double} value for the variance, \eqn{\sigma ^2}{sigma^2}, of a WN process.
 #' @return An S3 object with called ts.model with the following structure:
 #' \itemize{
 #'  \item{desc}{"AR1"}
-#'  \item{theta}{\eqn{phi}{\phi}, \eqn{sigma}{\sigma}}
+#'  \item{theta}{\eqn{\phi}{phi}, \eqn{\sigma}{sigma}}
+#'  \item{plength}{Number of Parameters}
+#'  \item{obj.desc}{y desc replicated x times}
+#'  \item{obj}{Depth of Parameters e.g. list(1,1)}
+#'  \item{adv}{Advanced Input TRUE or FALSE (e.g. specified value)}
 #' }
 #' @author JJB
-#' @details
-#' A standard deviation is required since the model generation statements utilize 
-#' randomization functions expecting a standard deviation instead of a variance.
-
 #' @examples
 #' AR1()
 #' AR1(phi=.32, sigma=1.3)
-AR1 = function(phi = 0.91, sigma = 1) {
-  if(length(phi) != 1 & length(sigma) != 1){
+AR1 = function(phi = NULL, sigma2 = NULL) {
+  adv = TRUE;
+  if(is.null(phi) || is.null(sigma2)){
+    phi = 0;
+    sigma2 = 1;
+    adv = FALSE;
+  }
+  if(length(phi) != 1 & length(sigma2) != 1){
     stop("Bad AR1 model submitted. Must be double values for two parameters.")
   }
-  out = structure(list(desc = "AR1",
-                       theta = c(phi,sigma),
-                       plength = 2), class = "ts.model")
+  out = structure(list(desc = c("AR1","SIGMA2"),
+                       theta = c(phi,sigma2),
+                       plength = 2,
+                       obj.desc = "AR1",
+                       obj = list(c(1,1)),
+                       adv = adv), class = "ts.model")
   invisible(out)
 }
 
 #' @title Create an Quantisation Noise (QN) Process
 #' @description Sets up the necessary backend for the QN process.
-#' @usage QN(q2)
 #' @param q2 A \code{double} value for the \eqn{Q^2}{Q^2} of a QN process.
 #' @return An S3 object with called ts.model with the following structure:
 #' \itemize{
 #'  \item{desc}{"QN"}
 #'  \item{theta}{\eqn{Q^2}{Q^2}}
+#'  \item{plength}{Number of Parameters}
+#'  \item{obj.desc}{y desc replicated x times}
+#'  \item{obj}{Depth of Parameters e.g. list(1)}
+#'  \item{adv}{Advanced Input TRUE or FALSE (e.g. specified value)}
 #' }
 #' @author JJB
-#' @details
-#' A standard deviation is required since the model generation statements utilize 
-#' randomization functions expecting a standard deviation instead of a variance.
-
 #' @examples
 #' QN()
 #' QN(q2=3.4)
-QN = function(q2 = 1) {
+QN = function(q2 = NULL) {
+  adv=TRUE
+  if(is.null(q2)){
+    q2 = 2
+    adv = FALSE
+  }
   if(length(q2) != 1){
     stop("Bad QN model submitted. Must be a double that indicates the Q2 value.")
   }
   out = structure(list(desc = "QN",
                        theta = q2,
-                       plength = 1), class = "ts.model")
+                       plength = 1,
+                       obj.desc = "QN",
+                       obj = list(1),
+                       adv = adv), class = "ts.model")
   invisible(out)
 }
 
 #' @title Create an White Noise (WN) Process
 #' @description Sets up the necessary backend for the WN process.
-#' @usage WN(sigma=1)
-#' @param sigma A \code{double} value for the standard deviation, \eqn{\sigma}{sigma}, of a WN process.
+#' @param sigma2 A \code{double} value for the variance, \eqn{\sigma ^2}{sigma^2}, of a WN process.
 #' @return An S3 object with called ts.model with the following structure:
 #' \itemize{
 #'  \item{desc}{"WN"}
 #'  \item{theta}{\eqn{\sigma}{sigma}}
+#'  \item{plength}{Number of Parameters}
+#'  \item{obj.desc}{y desc replicated x times}
+#'  \item{obj}{Depth of Parameters e.g. list(1)}
+#'  \item{adv}{Advanced Input TRUE or FALSE (e.g. specified value)}
 #' }
 #' @author JJB
-#' @details
-#' A standard deviation is required since the model generation statements utilize 
-#' randomization functions expecting a standard deviation instead of a variance.
-
 #' @examples
 #' WN()
 #' WN(sigma=3.4)
-WN = function(sigma = 1) {
-  if(length(sigma) != 1){
+WN = function(sigma2 = NULL) {
+  adv=TRUE
+  if(is.null(sigma2)){
+    sigma2 = 3
+    adv = FALSE
+  }
+  if(length(sigma2) != 1){
     stop("Bad WN model submitted. Must be a double that indicates the standard deviation.")
   }
   out = structure(list(desc = "WN",
-                       theta = sigma,
-                       plength = 1), class = "ts.model")
+                       theta = sigma2,
+                       plength = 1,
+                       obj.desc = "WN",
+                       obj = list(1),
+                       adv = adv), class = "ts.model")
   invisible(out)
 }
 
 #' @title Create an Random Walk (RW) Process
 #' @description Sets up the necessary backend for the RW process.
-#' @usage RW(sigma=1)
-#' @param sigma A \code{double} value for the standard deviation, \eqn{\sigma}{sigma}, of a RW process.
+#' @param sigma2 A \code{double} value for the variance, \eqn{\sigma ^2}{sigma^2}, of a WN process.
 #' @return An S3 object with called ts.model with the following structure:
 #' \itemize{
 #'  \item{desc}{"RW"}
 #'  \item{theta}{\eqn{\sigma}{sigma}}
+#'  \item{plength}{Number of Parameters}
+#'  \item{obj.desc}{y desc replicated x times}
+#'  \item{obj}{Depth of Parameters e.g. list(1)}
+#'  \item{adv}{Advanced Input TRUE or FALSE (e.g. specified value)}
 #' }
-#' @details
-#' A standard deviation is required since the model generation statements utilize 
-#' randomization functions expecting a standard deviation instead of a variance.
 #' @author JJB
-
 #' @examples
 #' RW()
 #' RW(sigma=3.4)
-RW = function(sigma = 1) {
-  if(length(sigma) != 1){
+RW = function(sigma2 = NULL) {
+  adv=TRUE
+  if(is.null(sigma2)){
+    sigma2 = 4
+    adv = FALSE
+  }
+  if(length(sigma2) != 1){
     stop("Bad RW model submitted. Must be a double that indicates the standard deviation.")
   }
   out = structure(list(desc = "RW",
-                       theta = sigma,
-                       plength = 1), class = "ts.model")
+                       theta = sigma2,
+                       plength = 1,
+                       obj.desc = "RW",
+                       obj = list(1),
+                       adv = adv), class = "ts.model")
   invisible(out)
 }
 
 #' @title Create an Drift (DR) Process
 #' @description Sets up the necessary backend for the DR process.
-#' @usage DR(slope = 5)
 #' @param slope A \code{double} value for the slope of a DR process.
 #' @return An S3 object with called ts.model with the following structure:
 #' \itemize{
 #'  \item{desc}{"DR"}
 #'  \item{theta}{slope}
+#'  \item{plength}{Number of Parameters}
+#'  \item{obj.desc}{y desc replicated x times}
+#'  \item{obj}{Depth of Parameters e.g. list(1)}
+#'  \item{adv}{Advanced Input TRUE or FALSE (e.g. specified value)}
 #' }
 #' @author JJB
-
 #' @examples
 #' DR()
 #' DR(slope=3.4)
-DR = function(slope = 5) {
+DR = function(slope = NULL) {
+  adv=TRUE
+  if(is.null(slope)){
+    slope = 5
+    adv = FALSE
+  }
   if(length(slope) != 1){
     stop("Bad Drift model submitted. Must be a double that indicates a slope.")
   }
   out = structure(list(desc = "DR",
                        theta = slope,
-                       plength = 1), class = "ts.model")
+                       plength = 1,
+                       obj.desc = "DR",
+                       obj = list(1),
+                       adv = adv), class = "ts.model")
   invisible(out)
 }
 
 #' @title Create an Autoregressive Moving Average (ARMA) Process
 #' @description Sets up the necessary backend for the ARMA process.
-#' @usage ARMA(ar = 1, ma = 1, sigma = 1.0)
 #' @param ar A \code{vector} or \code{integer} containing either the coefficients for \eqn{\phi}{phi}'s or the process number \eqn{p} for the Autoregressive (AR) term.
 #' @param ma A \code{vector} or \code{integer} containing either the coefficients for \eqn{\theta}{theta}'s or the process number \eqn{q} for the Moving Average (MA) term.
-#' @param sigma A \code{double} value for the standard deviation, \eqn{\sigma}{sigma}, of the ARMA process.
+#' @param sigma2 A \code{double} value for the standard deviation, \eqn{\sigma}{sigma}, of the ARMA process.
 #' @return An S3 object with called ts.model with the following structure:
 #' \itemize{
 #'  \item{desc}{\eqn{AR*p}{AR x p}, \eqn{MA*q}{MA x q}}
 #'  \item{theta}{\eqn{\sigma}{sigma}}
+#'  \item{plength}{Number of Parameters}
+#'  \item{obj.desc}{y desc replicated x times}
+#'  \item{obj}{Depth of Parameters e.g. list(c(length(ar),length(ma),1) )}
+#'  \item{adv}{Advanced Input TRUE or FALSE (e.g. specified value)}
 #' }
 #' @details
 #' A standard deviation is required since the model generation statements utilize 
 #' randomization functions expecting a standard deviation instead of a variance.
 #' @author JJB
-
 #' @examples
 #' # Create an ARMA(1,2) process
 #' ARMA(ar=1,2)
@@ -168,17 +210,22 @@ DR = function(slope = 5) {
 #' ARMA(ar=c(0.23,.43, .59), ma=c(0.4,.3))
 #' 
 #' # Creates an ARMA(3,2) process with predefined coefficients and standard deviation
-#' ARMA(ar=c(0.23,.43, .59), ma=c(0.4,.3), sigma = 1.5)
-ARMA = function(ar = 1, ma = 1, sigma = 1.0) {
+#' ARMA(ar=c(0.23,.43, .59), ma=c(0.4,.3), sigma2 = 1.5)
+ARMA = function(ar = 1, ma = 1, sigma2 = 1.0) {
+  adv = TRUE
   if(length(ar) == 1 & length(ma) == 1){
     if(is.whole(ar) & is.whole(ma)){
-      ar = rep(0, ar)
-      ma = rep(1, ma)
+      ar = rep(-1, ar)
+      ma = rep(-2, ma)
+      adv = FALSE
     }
   }
   out = structure(list(desc = c(rep("AR", length(ar)), rep("MA",length(ma)), "SIGMA2"),
-                       theta = c(ar, ma, sigma),
-                       plength = length(ar)+length(ma) + 1), class = "ts.model")
+                       theta = c(ar, ma, sigma2),
+                       plength = length(ar)+length(ma) + 1,
+                       obj.desc = "ARMA",
+                       obj = list(c(length(ar),length(ma),1)),
+                       adv = adv), class = "ts.model")
   invisible(out)
 }
 
@@ -191,6 +238,10 @@ ARMA = function(ar = 1, ma = 1, sigma = 1.0) {
 #' \itemize{
 #'  \item{desc}{y desc replicated x times}
 #'  \item{theta}{y theta replicated x times}
+#'  \item{plength}{Number of Parameters}
+#'  \item{obj.desc}{y desc replicated x times}
+#'  \item{obj}{Depth of Parameters e.g. list(c(1,1),c(1,1))}
+#'  \item{adv}{Advanced Input TRUE or FALSE (e.g. specified value)}
 #' }
 #' @author JJB
 #' @export
@@ -207,7 +258,10 @@ ARMA = function(ar = 1, ma = 1, sigma = 1.0) {
   }
   out = structure(list(desc = rep(y$desc,x),
                        theta = rep(y$theta,x),
-                       plength = rep(y$plength,x)), class = "ts.model")
+                       plength = rep(y$plength,x),
+                       obj.desc = rep(y$obj.desc,x),
+                       obj = rep(y$obj,x),
+                       adv = y$adv), class = "ts.model")
   invisible(out)
 }
 
@@ -220,6 +274,9 @@ ARMA = function(ar = 1, ma = 1, sigma = 1.0) {
 #' \itemize{
 #'  \item{desc}{combined x, y desc}
 #'  \item{theta}{combined x, y theta}
+#'  \item{plength}{Number of Parameters}
+#'  \item{obj}{Depth of Parameters e.g. list(1, c(1,1), c(length(ar),length(ma),1) )}
+#'  \item{adv}{Advanced Input TRUE or FALSE (e.g. specified value)}
 #' }
 #' @author JJB
 #' @export
@@ -227,9 +284,16 @@ ARMA = function(ar = 1, ma = 1, sigma = 1.0) {
 #' DR()+WN()
 #' AR1(phi=.3,sigma=.2)
 "+.ts.model" = function(x, y) {
+  adv = FALSE
+  if(y$adv & x$adv){
+    adv = TRUE
+  }
   out = structure(list(desc = c(x$desc, y$desc),
                        theta = c(x$theta,y$theta),
-                       plength = c(x$plength, y$plength)), class = "ts.model")
+                       plength = c(x$plength, y$plength),
+                       obj.desc = c(x$obj.desc, y$obj.desc),
+                       obj = c(x$obj, y$obj),
+                       adv = adv), class = "ts.model")
   invisible(out)
 }
 
@@ -246,4 +310,18 @@ ARMA = function(ar = 1, ma = 1, sigma = 1.0) {
 #' @author JJB
 #' @examples
 #' DR()+WN()+RW()+AR1()+ARMA(1,2)
-print.ts.model = function(x, ...) cat("Desc:\n", x$desc, "\nValues:\n", x$theta)
+print.ts.model = function(x, ...){
+
+  desctable = cbind(x$desc, x$theta);
+  colnames(desctable) = c("Terms", "Value")
+  cat("\nAdvanced Enabled:", x$adv, "\n")
+  if(!x$adv){
+    cat("The program will attempt to guess starting values for...\n")
+    print(desctable[,1])
+    cat("This model will only work using the gmwm() function.\n",
+        "To have the option of using adv.gmwm(), please supply values for each parameter.\n")
+  }else{
+    print(desctable)
+    cat("This model is able to be supplied to both gmwm() or adv.gmwm()\n")
+  }
+}
