b3096ddbeaf27d879a953f51da84f83e70fc1acf
coatless <balamut2@illinois.edu>
2015-03-25 05:14:54 -0500

    Updated process_to_wv with new theoretical_wv function.

--- src/process_to_wv.cpp
+++ src/process_to_wv.cpp
@@ -63,8 +63,8 @@ arma::vec arma_to_wv(arma::vec ar, arma::vec ma, arma::vec tau, double sigma) {
 //' wv.theo = qn_to_wv(1, tau)
 //' plot(tau, wv.theo, col = "red")
 // [[Rcpp::export]]
-arma::vec qn_to_wv(double q2, const arma::vec& Tau){
-  return 3*q2/(2*arma::square(Tau));
+arma::vec qn_to_wv(double q2, const arma::vec& tau){
+  return 3*q2/(2*arma::square(tau));
 }
 
 //' @title White Noise to WV
@@ -79,8 +79,8 @@ arma::vec qn_to_wv(double q2, const arma::vec& Tau){
 //' wv.theo = wn_to_wv(1, tau)
 //' plot(tau, wv.theo, col = "red")
 // [[Rcpp::export]]
-arma::vec wn_to_wv(double sig2, arma::vec Tau){
-  return sig2/Tau;
+arma::vec wn_to_wv(double sig2, arma::vec tau){
+  return sig2/tau;
 }
 
 
@@ -96,8 +96,8 @@ arma::vec wn_to_wv(double sig2, arma::vec Tau){
 //' wv.theo = rw_to_wv(1,tau)
 //' plot(tau, wv.theo, col = "red")
 // [[Rcpp::export]]
-arma::vec rw_to_wv(double sig2, const arma::vec& Tau){
-  return sig2*((2*arma::square(Tau) + 2)/(24*Tau));
+arma::vec rw_to_wv(double sig2, const arma::vec& tau){
+  return sig2*((2*arma::square(tau) + 4)/(24*tau));
 }
 
 
@@ -113,15 +113,15 @@ arma::vec rw_to_wv(double sig2, const arma::vec& Tau){
 //' wv.theo = dr_to_wv(1, tau)
 //' plot(tau, wv.theo, col = "red")
 // [[Rcpp::export]]
-arma::vec dr_to_wv(double omega,const arma::vec& Tau){
-	return square(omega)*arma::square(Tau)/16;
+arma::vec dr_to_wv(double omega,const arma::vec& tau){
+	return square(omega)*arma::square(tau)/16;
 }
 
 //' @title AR1 process to WV
 //' @description This function compute the WV (haar) of an AR(1) process
 //' @param phi A \code{double} that is the phi term of the AR(1) process
 //' @param sig2 A \code{double} corresponding to variance of AR(1) process
-//' @param Tau A \code{vec} containing the scales e.g. 2^tau
+//' @param tau A \code{vec} containing the scales e.g. 2^tau
 //' @return A \code{vec} containing the wavelet variance of the AR(1) process.
 //' @examples
 //' x.sim = gen_ar1( N = 10000, phi = 0.9, sigma2 = 4 )
@@ -130,15 +130,15 @@ arma::vec dr_to_wv(double omega,const arma::vec& Tau){
 //' wv.theo = ar1_to_wv(phi = 0.9, sig2 = 16, tau)
 //' plot(tau, wv.theo, col = "red")
 // [[Rcpp::export]]
-arma::vec ar1_to_wv(double phi, double sig2, const arma::vec& Tau){
-  unsigned int size_tau = Tau.n_elem;
+arma::vec ar1_to_wv(double phi, double sig2, const arma::vec& tau){
+  unsigned int size_tau = tau.n_elem;
   arma::vec temp_term(size_tau);
   arma::vec temp_term_redux(size_tau);
   for(unsigned int i=0; i< size_tau; i++){
-    temp_term(i) = 4*pow(phi,(Tau(i)/2 + 1));
-    temp_term_redux(i) = pow(phi,(Tau(i)+1));
+    temp_term(i) = 4*pow(phi,(tau(i)/2 + 1));
+    temp_term_redux(i) = pow(phi,(tau(i)+1));
   }
-	return ((Tau/2 - 3*phi - Tau/2*pow(phi,2) + temp_term - temp_term_redux)/(arma::square(Tau/2)*pow(1-phi,2)*(1-pow(phi,2)))*sig2)/2;
+	return ((tau/2 - 3*phi - tau/2*pow(phi,2) + temp_term - temp_term_redux)/(arma::square(tau/2)*pow(1-phi,2)*(1-pow(phi,2)))*sig2)/2;
 }
 
 //' @title Model Process to WV
@@ -156,49 +156,82 @@ arma::vec ar1_to_wv(double phi, double sig2, const arma::vec& Tau){
 //' wv.theo = ar1_to_wv(phi = 0.9, sig2 = 16, tau)
 //' plot(tau, wv.theo, col = "red")
 // [[Rcpp::export]]
-arma::vec theo_wv(const arma::vec& theta, const std::vector<std::string>& desc,
-                                const arma::vec& tau, int N){
+arma::vec theoretical_wv(const arma::vec& theta, 
+                         const std::vector<std::string>& desc,
+                         const arma::field<arma::vec>& objdesc, const arma::vec& tau){
   
   unsigned int num_desc = desc.size();
-
   arma::vec wv_theo = arma::zeros<arma::vec>(tau.n_elem);
- 
-  // Loop Counters
-  
+    
   unsigned int i_theta = 0;
   for(unsigned int i = 0; i < num_desc; i++){
-    double theta_est = theta(i_theta);
-
-    // ARMA is needed!
+    
+    // Add ARMA
+  
+    double theta_value = theta(i_theta);
+    
+    std::string element_type = desc[i];
     
     // AR 1
-    if(desc[i] == "AR1"){
+    if(element_type == "AR1"){
+
       ++i_theta;
       double sig2 = theta(i_theta);
       
       // Compute theoretical WV
-      wv_theo += ar1_to_wv(theta_est,sig2,tau);
+      wv_theo += ar1_to_wv(theta_value, sig2, tau);
     }
-    // RW
-    else if(desc[i] == "RW"){
-      wv_theo += rw_to_wv(theta_est,tau);
+    else if(element_type == "ARMA"){
+      
+      arma::vec model_params = objdesc(i);
+      
+      unsigned int p = model_params(0);
+      unsigned int q = model_params(1);
+      
+      arma::vec ar;
+      arma::vec ma;
+      
+
+      if(p == 0){
+        ar = arma::zeros<arma::vec>(0);
+      }else{
+        ar = theta.rows(i_theta,i_theta+p-1);
+      }
+      
+      i_theta += p;
+      
+      if(q == 0){
+        ma = arma::zeros<arma::vec>(0); 
+      }else{
+        ma = theta.rows(i_theta,i_theta+q-1);
+      }
+      
+      i_theta += q;
+
+      
+      wv_theo += arma_to_wv(ar, ma, tau, exp(theta(i_theta)) );
     }
     // DR
-    else if(desc[i] == "DR"){
-      wv_theo += dr_to_wv(theta_est,tau);
+    else if(element_type == "DR"){
+      wv_theo += dr_to_wv(theta_value, tau);
+    }
+    // QN
+    else if(element_type == "QN"){
+      wv_theo += qn_to_wv(theta_value, tau);
+    }
+    // RW
+    else if(element_type == "RW"){
+      wv_theo += rw_to_wv(theta_value, tau);
     }
     // WN
     else{
-      wv_theo += wn_to_wv(theta_est,tau);
+      wv_theo += wn_to_wv(theta_value, tau);
     }
     
-    // Increase counter
-     ++i_theta;
+    ++i_theta;
   }
 
   return wv_theo;
 }
-
-
 
 /* ------------------------------ End Process to WV Functions --------------------------------- */
