bc7d6ab90c4a1aa6d5697b48364b417649e5cc4a
Stephane <stephane@illinois.edu>
2015-03-26 15:50:57 -0500

    Minor change in autoplot.wvar to remove lines around conf region.
    Former-commit-id: b6f2015e08fa706f3a9eb42b07fedd4c4bcad309

--- src/covariance_matrix.h
+++
@@ -1,7 +0,0 @@
-#ifndef COVARIANCE_MATRIX
-#define COVARIANCE_MATRIX
-
-arma::field<arma::mat> compute_cov_cpp(arma::field<arma::vec> signal_modwt, unsigned int nb_level, std::string compute_v,
-                                        bool robust, double eff);
-
-#endif
--- src/inline_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef INLINE_FUNCTIONS
-#define INLINE_FUNCTIONS
-
-inline double square(double x){
-  return x*x;
-}
-
-#endif
