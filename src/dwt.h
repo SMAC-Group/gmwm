efe28146d6d35631f819019a43828aaa7f4e85d0
coatless <james.balamuta@gmail.com>
2015-03-27 00:52:38 -0500

    Added newline at the end of header files...
    Former-commit-id: d2f72964f571f529c607c7de0778cb32574b6dd7

--- src/dwt.h
+++ src/dwt.h
@@ -1,5 +1,6 @@
 #ifndef DWT
 #define DWT
+
 arma::field<arma::vec> dwt_cpp(arma::vec x, std::string filter_name, 
                                  unsigned int nlevels, std::string boundary);
                                  
@@ -7,4 +8,5 @@ arma::field<arma::vec> modwt_cpp(arma::vec x, std::string filter_name,
                                    unsigned int nlevels, std::string boundary);
                                    
 arma::field<arma::vec> brick_wall(arma::field<arma::vec> x,  arma::field<arma::vec> wave_filter, std::string method);
+
 #endif
