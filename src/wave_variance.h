edeae54c4d963aaf92fac00990509e132cc582c8
James J Balamuta <james.balamuta@gmail.com>
2015-04-06 23:41:39 -0500

    Switched wave_variance from p to alpha
    and standardized alpha to be the normal input
    
    e.g. 95% => alpha = 0.05
    
    vs. before
    p = 0.025
    
    
    Former-commit-id: e70199be2618c416117bbe680919add2d5a35d52

--- src/wave_variance.h
+++ src/wave_variance.h
@@ -1,18 +1,18 @@
 #ifndef WAVE_VARIANCE
 #define WAVE_VARIANCE
 
-arma::mat ci_eta3(arma::vec y,  arma::vec dims, double p);
+arma::mat ci_eta3(arma::vec y,  arma::vec dims, double alpha_ov_2);
 
-arma::mat ci_eta3_robust(arma::vec y, arma::vec dims, double p, double eff);
+arma::mat ci_eta3_robust(arma::vec y, arma::vec dims, double alpha_ov_2, double eff);
 
 arma::mat ci_wave_variance(const arma::field<arma::vec>& signal_modwt_bw, const arma::vec& y, 
-                            std::string type, double p, bool robust, double eff);
+                            std::string type, double alpha_ov_2, bool robust, double eff);
  
 arma::vec wave_variance(const arma::field<arma::vec>& signal_modwt_bw,
                         bool robust, double eff);
 
 arma::mat wvar_cpp(const arma::field<arma::vec>& signal_modwt, 
-                   bool robust, double eff, double p, 
+                   bool robust, double eff, double alpha, 
                    std::string ci_type, std::string strWavelet);
 
 arma::vec scales_cpp(unsigned int nb_level);
