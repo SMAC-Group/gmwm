9729130e3aa2ef0bb306065c1ea7c2520a2ba807
coatless <balamut2@illinois.edu>
2015-05-18 17:44:55 -0500

    moved is.whole to own file
    Former-commit-id: 99f1b964e52abc7daa1bb12a37f30307523cc338

--- R/ts.model.R
+++ R/ts.model.R
@@ -1,17 +1,3 @@
-#' @title Integer Check
-#' @description Checks whether the submitted value is an integer
-#' @usage is.whole(x)
-#' @param x A \code{numeric} value to check to see if it is an integer.
-#' @return A \code{boolean} value indicating whether the value is an integer or not.
-#' @author JJB
-#' @examples
-#' is.whole(2.3)
-#' is.whole(4)
-#' is.whole(c(1,2,3))
-#' is.whole(c(.4,.5,.6))
-#' is.whole(c(7,.8,9))
-is.whole = function(x){ is.numeric(x) && floor(x)==x } 
-
 #' @title Create an Autoregressive 1 [AR(1)] Process
 #' @description Setups the necessary backend for the AR1 process.
 #' @param phi A \code{double} value for the \eqn{\phi}{phi} of an AR1 process.
