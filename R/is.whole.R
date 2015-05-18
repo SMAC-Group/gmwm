99f1b964e52abc7daa1bb12a37f30307523cc338
coatless <balamut2@illinois.edu>
2015-05-18 17:44:55 -0500

    moved is.whole to own file

---
+++ R/is.whole.R
@@ -0,0 +1,13 @@
+#' @title Integer Check
+#' @description Checks whether the submitted value is an integer
+#' @usage is.whole(x)
+#' @param x A \code{numeric} value to check to see if it is an integer.
+#' @return A \code{boolean} value indicating whether the value is an integer or not.
+#' @author JJB
+#' @examples
+#' is.whole(2.3)
+#' is.whole(4)
+#' is.whole(c(1,2,3))
+#' is.whole(c(.4,.5,.6))
+#' is.whole(c(7,.8,9))
+is.whole = function(x){ is.numeric(x) && floor(x)==x } 
