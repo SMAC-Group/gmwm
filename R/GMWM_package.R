a82ab9d6f4205869b4de8f7ccef051df03f465ae
coatless <balamut2@illinois.edu>
2015-05-25 19:08:45 -0500

    Updated version to 0.11.5
    Former-commit-id: bf1371479ef5397b2083123627b6b3c55af8f002

--- R/GMWM_package.R
+++ R/GMWM_package.R
@@ -6,8 +6,8 @@
 #' \tabular{ll}{
 #' Package: \tab GMWM\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.7.0\cr
-#' Date: \tab 2015-02-28\cr
+#' Version: \tab 0.11.5\cr
+#' Date: \tab 2015-05-25\cr
 #' License: \tab MIT + file LICENSE\cr
 #' }
 #' Some details should be added  
