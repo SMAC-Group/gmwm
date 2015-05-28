8f7d0d4db9cbcd74822d0ab895d141e5d7082f13
coatless <balamut2@illinois.edu>
2015-05-28 16:36:43 -0500

    updated imu data documentation
    added test functions of map manipulation to ts_checks.
    
    
    Former-commit-id: 9b9fb150b50d841bb1d15119b94b2fa68d96d452

--- R/imu.R
+++ R/imu.R
@@ -3,15 +3,14 @@
 #' @description This data set contains gyroscope data
 #' @docType data
 #' @usage imu
-#' @format A \code{data.frame} with 873,684 observations and 7 variables. The variables are as follows:
+#' @format A \code{data.frame} with 873,684 observations and 6 variables. The variables are as follows:
 #' \itemize{
-#'  \item{V1}{Time}
-#'  \item{V2}{X-Axis}
-#'  \item{V3}{Y-Axis}
-#'  \item{V4}{Z-Axis}
-#'  \item{V5}{Fill in}
-#'  \item{V6}{Fill in}
-#'  \item{V7}{Fill in}
+#'  \item{Gyro. X}{Gyroscope X-Axis}
+#'  \item{Gyro. Y}{Gyroscope Y-Axis}
+#'  \item{Gyro. Z}{Gyroscope Z-Axis}
+#'  \item{Accel. X}{Accelerometer X-Axis}
+#'  \item{Accel. Y}{Accelerometer Y-Axis}
+#'  \item{Accel. Z}{Accelerometer Z-Axis}
 #' }
 #' @source Where's the data frame?
 #' @author JJB
