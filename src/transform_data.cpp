dbceb23a38584439a6d44e0780206e0f58fed99a
coatless <balamut2@illinois.edu>
2015-05-31 20:23:30 -0500

    Rewrote arma section to include an identity link for ARMA under ssm and IMU to use psuedo link.

--- src/transform_data.cpp
+++ src/transform_data.cpp
@@ -12,7 +12,7 @@ using namespace Rcpp;
 //' pseudo_logit_inv(x.sim)
 // [[Rcpp::export]]
 arma::vec pseudo_logit_inv(const arma::vec& x){
-  return 2*exp(x)/(1 + exp(x)) -1;
+  return 2/(1 + exp(-x)) - 1;
 }
 
 //' @title Logit Inverse Function
@@ -61,59 +61,85 @@ double logit(double x){
   return log(x/(1 - x));
 }
 
+
+
+//' @title Logit Function
+//' @description This function computes the logit link function.
+//' @param x A \code{vec} containing probabilities (e.g. -1 <= x <= 1)
+//' @return A \code{vec} containing logit terms.
+//' @examples
+//' x.sim = runif(100)
+//' logit(x.sim)
+// [[Rcpp::export]]
+arma::vec logit2(const arma::vec& x){
+  return log(x/(1 - x));
+}
+
+double logit2(double x){
+  return log(x/(1 - x));
+}
+
+
+
 //' @title Transform Values for Optimization
 //' @description Transform parameter guesses prior to estimating with GMWM
 //' @return A \code{vec} containing the transformed guesses.
 // [[Rcpp::export]]
 arma::vec transform_values(const arma::vec& theta,
                            const std::vector<std::string>& desc, const arma::field<arma::vec>& objdesc, std::string model_type){
-    arma::vec starting  = arma::zeros<arma::vec>(theta.n_elem);
-    unsigned int i_theta = 0;
-    unsigned int num_desc = desc.size();
-    
-    for(unsigned int i = 0; i < num_desc; i++){
-      // AR 1
-      if(desc[i] == "AR1" ){
-        
-        // Apply model specific parameter transformation
-        if(model_type == "imu"){
-  	      starting(i_theta) = arma::as_scalar(logit(theta.row(i_theta)));
-        }else{ // O.W. SSM case
-          starting(i_theta) = arma::as_scalar(pseudo_logit(theta.row(i_theta)));
-        }
-        
-        // Increase index for phi term
-  	    ++i_theta;
-        
-        // Handle the SIGMA2 term
-  	    starting(i_theta) = log(theta(i_theta));
-        
-  	  }
-      else if( desc[i] == "ARMA" ) {
-        
-        arma::vec arma_params = objdesc(i);
-        
-        unsigned int p = arma_params(0); // AR(P)
-        unsigned int q = arma_params(1); // MA(Q)
-        
-        unsigned int param_space = i_theta + p + q;
-        
-        // Change pseudo_logit to logit based on model type??
-        starting.rows(i_theta, param_space - 1) = pseudo_logit(theta.rows(i_theta, param_space - 1));
-        
-        // Increment index
-        i_theta += param_space;
-        
-        // Take care of SIGMA2 term
-        starting(param_space) = log(theta(param_space));
-
+  arma::vec starting  = arma::zeros<arma::vec>(theta.n_elem);
+  unsigned int i_theta = 0;
+  unsigned int num_desc = desc.size();
+  
+  for(unsigned int i = 0; i < num_desc; i++){
+    // AR 1
+    if(desc[i] == "AR1" ){
+      
+      // Apply model specific parameter transformation
+      if(model_type == "imu"){
+        starting(i_theta) = arma::as_scalar(logit(theta.row(i_theta)));
+      }else{ // O.W. SSM case
+        starting(i_theta) = arma::as_scalar(pseudo_logit(theta.row(i_theta)));
       }
-  	  else{
-        starting(i_theta) = log(theta(i_theta));
-  	  }
+      
+      // Increase index for phi term
       ++i_theta;
-  }  
+    }
+    else if( desc[i] == "ARMA" ) {
+      
+      // Obtain the ARMA Model information
+      arma::vec arma_params = objdesc(i);
+      
+      // Get the count of parameter values
+      unsigned int p = arma_params(0); // AR(P)
+      unsigned int q = arma_params(1); // MA(Q)
+      
+      unsigned int param_est = i_theta + p + q;
+      
+      // Determine transformation to apply
+      if(model_type == "imu"){
+        
+        // Use the pseudo logit function (0 <= x <= 1) => R
+        starting.rows(i_theta, param_est - 1) = pseudo_logit(theta.rows(i_theta, param_est - 1));
+        
+      }
+      else{ 
+        // SSM Case
+        starting.rows(i_theta, param_est - 1) = theta.rows(i_theta, param_est- 1);
+      }
+      
+      // Increment index
+      i_theta = param_est;
+      
+      
+    }
     
+    // Take care of SIGMA2 term
+    starting(i_theta) = log(theta(i_theta));
+    
+    ++i_theta;
+  }  
+
   return starting;
 }
 
@@ -138,30 +164,36 @@ arma::colvec untransform_values(const arma::vec& theta,
           result(i_theta) = arma::as_scalar(pseudo_logit_inv(theta.row(i_theta)));
         }
   	    ++i_theta;
-  	    result(i_theta) = exp(theta(i_theta));
   	  }
       else if(element_type == "ARMA" ) {
         
+        // Obtain the ARMA Model information
         arma::vec arma_params = objdesc(i);
         
+        // Get the count of parameter values
         unsigned int p = arma_params(0); // AR(P)
         unsigned int q = arma_params(1); // MA(Q)
         
-        unsigned int param_space = i_theta + p + q;
+        unsigned int param_est = i_theta + p + q;
         
-        // Change pseudo_logit_inv to logit_inv based on model type??
-        result.rows(i_theta, param_space - 1) = pseudo_logit_inv(theta.rows(i_theta, param_space - 1));
-        
-        // Increment index to account for P+Q values
-        i_theta += param_space;
-        
-        // Take care of SIGMA2 term
-        result(param_space) = exp(theta(param_space));
+        // Determine transformation to apply
+        if(model_type == "imu"){
+          
+          // Use the invs pseudo logit function R => (0 <= x <= 1) 
+          result.rows(i_theta, param_est - 1) = pseudo_logit_inv(theta.rows(i_theta, param_est - 1));
 
+        }else{ 
+          // SSM Case
+          result.rows(i_theta, param_est - 1) = theta.rows(i_theta, param_est - 1);
+        }
+        
+        // Increment index
+        i_theta = param_est;
+        
       }
-  	  else {
-        result(i_theta) = exp(theta(i_theta));
-  	  }
+
+      // Take care of SIGMA2 term or other term...
+      result(i_theta) = exp(theta(i_theta));
       
       ++i_theta;
   }  
