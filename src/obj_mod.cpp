bde41f425b4a16917b48021b661833bff2a4fc80
coatless <balamut2@illinois.edu>
2015-06-05 20:02:27 -0500

    Extract parts of the object and return as a vector.
    Former-commit-id: 8026368412800832d4a8e609c2ec3e4f6d04d170

---
+++ src/obj_mod.cpp
@@ -0,0 +1,30 @@
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+
+
+//' @title Extract Object
+//' @description Extracts the object information and returns it.
+//' @param theta A \code{vec} containing the theta values.
+//' @param objdesc A \code{vec} at the desc point.
+//' @param cur_position An \code{integer} at the current position.
+//' @return A \code{field<vec>} containing the breakdown of the object.
+// [[Rcpp::export]]
+arma::field<arma::vec> obj_extract(arma::vec theta,
+                                   arma::vec objdesc,
+                                   unsigned int& cur_position) {
+  
+  
+  unsigned int nobs = objdesc.n_elem;
+  
+  arma::field<arma::vec> out(nobs);
+  
+  unsigned int i;
+  
+  for(i = 0; i < nobs; i++){
+    unsigned int obj = objdesc(i);
+    out(i) = theta.rows(cur_position, cur_position + obj - 1);
+    cur_position += obj;
+  }
+  
+  return out;
+}
