9ad0157f32ddc487d968092c9d2869c69e694597
coatless <balamut2@illinois.edu>
2015-06-17 17:44:22 -0500

    Orders duplicate AR1 terms in descending order.
    This makes simulation studies possible...
    
    
    Former-commit-id: f9d35ab8f001c1a2b6669250926fbba44ee3d939

--- src/ts_checks.h
+++ src/ts_checks.h
@@ -7,4 +7,6 @@ bool invert_check(const arma::vec& x);
 
 std::map<std::string, int> count_models(const std::vector<std::string>& desc);
 
+arma::vec order_AR1s(arma::vec theta, const std::vector<std::string>& desc, const arma::field<arma::vec> objdesc);
+
 #endif
