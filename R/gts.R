f62435407b581106dbf30b2c8999d99862e8bf6e
Wenchao Yang <wenchao.yang10@hotmail.com>
2015-08-28 02:39:41 -0500

    Changed gen.ts() to gen.gts(), and deleted file gen.ts.R Changed gen.gts(), gts(), autoplot.gts(), plot.gts() according to the new structure of gts object. Changed convert.gts() to unitConversion(), added new units 'ns', 'ms', and made it work for general case

--- R/gts.R
+++ R/gts.R
@@ -1,24 +1,26 @@
-#' @title Create a GMWM TS Object
+#' @title Create a GMWM TS Object based on data
 #' @description Setups a time series oriented object that works well with graphing and summary utilities
-#' @param data A one column matrix, data.frame, or numeric vector.
-#' @param name A string that provides an identifier to the data. If not supplied, the name of the first column will be used. Otherwise, it will be registered as "Dataset X"
-#' @param freq A number that provides the rate of samples.
-#' @param unit A string that contains the unit expression of the frequency. 
-#' @return An S3 object with called gts with the following structure:
+#' @param data A one column \code{matrix}, \code{data.frame}, or a numeric \code{vector}.
+#' @param x.lim If not \code{NULL}, it is a \code{vector} with two numeric values setting the limit on x-axis.
+#' @param freq A \code{numeric} that provides the rate of samples. Default value is 1.
+#' @param unit A \code{string} that contains the unit expression of the frequency. Default value is \code{NULL}.
+#' @param name A \code{string} that provides an identifier to the data. Default value is an empty string.
+#' @return A \code{gts} object with the structure:
 #' \itemize{
-#'  \item{data} {Data set information}
-#'  \item{name} {Name of the Dataset}
-#'  \item{freq} {Numeric representation of frequency}
-#'  \item{Unit} {String representation of the Unit}
+#'   \item{x:} {A \code{matirx} that contains x-axis values used to plot}
+#'   \item{data:} {A \code{matrix} that contains data for combined processes}
+#'   \item{freq:} {Numeric representation of frequency}
+#'   \item{unit:} {String representation of the unit}
+#'   \item{name:} {Name of the dataset}
 #' }
 #' @author JJB
 #' @examples
 #' m = data.frame(rnorm(5))
 #' gts(m)
 #' 
-#' x = gen.ts(WN(sigma2=1), 50)
+#' x = gen.gts(WN(sigma2=1), 50)$data
 #' x = gts(x)
-gts = function(data, name = "", freq = 1, unit = "sec"){
+gts = function(data, x.lim = NULL, freq = 1, unit = NULL, name = ""){
   
   # Force data.frame to matrix  
   if (is.data.frame(data)){ 
@@ -29,54 +31,196 @@ gts = function(data, name = "", freq = 1, unit = "sec"){
   if (is.matrix(data)) {
     ndata = nrow(data)
     colnames(data) = NULL
+    
+    #check ncol
+    ncolumn = ncol(data)
+    if(!ncolumn==1){
+      stop('data must have one column.')
+    }
   } else {
     ndata = length(data)
   }
   if (ndata == 0) {
-    stop("Not a valid data object! Please supply a data set with one column that is in either a data.frame, matrix,  or numeric object.")
+    stop("Not a valid data object! Please supply a data set with one column that is in either a data.frame, matrix, or numeric object.")
   }
   
-  if(!is(freq,"numeric")){
+  if(!is(freq,"numeric") || length(freq)!=1){
     stop("freq must be numeric")
   }
+  if(freq<=0) {stop('freq must be larger than 0.')}
   
-  if(!is(name,"character") || !is(unit,"character")){
-    stop("Name or unit is not a character")
+  if(!is.null(unit)){
+    if(!unit %in% c('ns', 'ms', 'sec', 'second', 'min', 'minute', 'hour', 'day', 'mon', 'month', 'year')){
+      stop('The supported units are "ns", "ms", "sec", "min", "hour", "day", "month", "year". ')
+    }
   }
   
-  x = structure(list(data = data, name = name, freq = freq, unit = unit), class = "gts")
-  invisible(x)
+  if(is.null(x.lim)){
+    x = seq(from = 0, to = (ndata-1), length.out = ndata)
+  }else{
+    if(class(x.lim)!="numeric" || length(x.lim)!=2){
+      stop('x.lim must be a numeric vector with 2 values.')}
+    if( !(x.lim[2] - x.lim[1])==(ndata-1)  ){
+      stop('x.lim cannot create a vector which has the same length of the data you supplied.')
+    }
+    x = seq(from = x.lim[1], to = x.lim[2], length.out = ndata)
+  }
+  
+  out = structure(list(
+    x = as.matrix(x),
+    data = as.matrix(data),
+    name = name, 
+    freq = freq, 
+    unit = unit), class = 'gts' )
+  
+  invisible(out)
+}
+
+
+#' @title Create a GMWM TS Object based on model
+#' @description Create a \code{gts} object based on a supplied time series model.
+#' @param model A \code{ts.model} or \code{gmwm} object containing one of the allowed models.
+#' @param N An \code{interger} containing the amount of observations for the time series.
+#' @param x.lim A \code{vector} with two numeric values setting the limit on x-axis.
+#' @param freq A \code{numeric} that provides the rate of samples. Default value is 1.
+#' @param unit A \code{string} that contains the unit expression of the frequency. Default value is \code{NULL}.
+#' @param name A \code{string} that provides an identifier to the data. Default value is an empty string.
+#' @return A \code{gts} object with the structure:
+#' \itemize{
+#'   \item{x:} {A \code{matirx} that contains x-axis values to plot}
+#'   \item{data:} {A \code{matrix} that contains data for combined processes}
+#'   \item{freq:} {Numeric representation of frequency}
+#'   \item{unit:} {String representation of the unit}
+#'   \item{name:} {Name of the Dataset}
+#' }
+#' @details
+#' This function accepts either a \code{ts.model} object (e.g. AR1(phi = .3, sigma2 =1) + WN(sigma2 = 1)) or a \code{gmwm} object.
+#' @examples
+#' # AR
+#' set.seed(1336)
+#' model = AR1(phi = .99, sigma = 1) + WN(sigma2=1)
+#' gen.gts(model)
+gen.gts = function(model, N = 1000, x.lim= c(0,N-1), freq = 1, unit = NULL, name = ""){
+  
+  # Do we have a valid model?
+  if(!(is(model, "ts.model") || is(model, "gmwm"))){
+    stop("model must be created from a ts.model or gmwm object using a supported component (e.g. AR1(), ARMA(p,q), DR(), RW(), QN(), and WN(). ")
+  }
+  
+  if(is(model,"gmwm")){
+    model = model$model.hat
+  }
+  
+  if(!is(freq,"numeric") || length(freq)!=1){
+    stop("freq must be numeric")
+  }
+  if(freq<=0) {stop('freq must be larger than 0.')}
+  
+  if(!is.null(unit)){
+    if(!unit %in% c('ns', 'ms', 'sec', 'second', 'min', 'minute', 'hour', 'day', 'mon', 'month', 'year')){
+      stop('The supported units are "ns", "ms", "sec", "min", "hour", "day", "month", "year". ')
+    }
+  }
+  
+  if(class(x.lim)!="numeric" || length(x.lim)!=2){
+    stop('x.lim must be a numeric vector with 2 values.')}
+  
+  x = seq(from = x.lim[1], to = x.lim[2], length.out = N)
+  
+  # Information Required by GMWM:
+  desc = model$desc
+  
+  obj = model$obj.desc
+  
+  # Identifiability issues
+  if(any( count_models(desc)[c("DR","QN","RW","WN")] >1)){
+    stop("Two instances of either: DR, QN, RW, or WN has been detected. As a result, the model will have identifiability issues. Please submit a new model.")
+  }
+  
+  if(!model$starting){
+    theta = model$theta
+    out = .Call('GMWM_gen_model', PACKAGE = 'GMWM', N, theta, desc, obj)
+  }else{
+    stop("Need to supply initial values within the ts.model object.")
+  }
+  
+  out = structure(list(
+    x = as.matrix(x),
+    data = as.matrix(out),
+    name = name, 
+    freq = freq, 
+    unit = unit), class = 'gts' )
+  
+  invisible(out)
+
 }
 
 #' @title Plot Time Series Data
 #' @description This function is implemented with ggplot2.
 #' @method plot gts
 #' @param x A \code{gts} object
+#' @param to.unit A \code{string} indicating the unit which the data is converted to. The supported units are "ns"(nanosecond), "ms"(millisecond), "sec", "min", "hour", "day", "month", and "year".
 #' @param background A \code{string} that determines the graph background. It can be \code{'grey'} or \code{'white'}.
 #' @param line.type A \code{string} that indicates the type of lines.
 #' @param line.color A \code{string} that indicates the color of lines.
-#' @param point.size A \code{integer} that indicates the size of points on lines.
-#' @param point.shape A \code{integer} that indicates the shape of points on lines.
+#' @param point.size An \code{integer} that indicates the size of points on lines.
+#' @param point.shape An \code{integer} that indicates the shape of points on lines.
 #' @param title A \code{string} that indicates the title of the graph.
 #' @param title.size An \code{integer} that indicates the size of title.
 #' @param axis.label.size An \code{integer} that indicates the size of label.
 #' @param axis.tick.size An \code{integer} that indicates the size of tick mark.
-#' @param to.unit A \code{string} indicating the unit which the data is converted to. It can be sec, min, hour, day, month and year.
 #' @param axis.x.label A \code{string} that indicates the label on x axis.
 #' @param axis.y.label A \code{string} that indicates the label on y axis.
 #' @param ... other arguments passed to specific methods
 #' @return A ggplot2 panel containing the graph of time series.
 #' @author Wenchao
 #' @examples
-#' x = gen.ts(WN(sigma2=1), 50)
-#' x = gts(x)
+#' x = gen.gts(WN(sigma2=1), 50)
 #' plot(x)
-plot.gts = function(x, background = 'white',  
+plot.gts = function(x, to.unit = NULL, background = 'white',  
                     line.type = 'solid', line.color = '#003C7D',
                     point.size = 0, point.shape = 20,
                     title = NULL, title.size= 15, 
-                    axis.label.size = 13, axis.tick.size = 11, to.unit = NULL,
-                    axis.x.label = 'Time', axis.y.label = deparse(substitute(x)), ... ){
+                    axis.label.size = 13, axis.tick.size = 11, 
+                    axis.x.label = NULL, axis.y.label = NULL, ... ){
+  
+  autoplot.gts(object = x, to.unit = to.unit, background = background,  
+               line.type = line.type, line.color = line.color,
+               point.size = point.size, point.shape = point.shape,
+               title = title, title.size= title.size, 
+               axis.label.size = axis.label.size, axis.tick.size = axis.tick.size, 
+               axis.x.label = axis.x.label, axis.y.label = axis.y.label )
+  
+}
+
+#' @title Plot Time Series Data
+#' @description This function is implemented with ggplot2.
+#' @method autoplot gts
+#' @param object A \code{gts} object
+#' @param to.unit A \code{string} indicating the unit which the data is converted to. The supported units are "ns"(nanosecond), "ms"(millisecond), "sec", "min", "hour", "day", "month", and "year".
+#' @param background A \code{string} that determines the graph background. It can be \code{'grey'} or \code{'white'}.
+#' @param line.type A \code{string} that indicates the type of lines.
+#' @param line.color A \code{string} that indicates the color of lines.
+#' @param point.size An \code{integer} that indicates the size of points on lines.
+#' @param point.shape An \code{integer} that indicates the shape of points on lines.
+#' @param title A \code{string} that indicates the title of the graph.
+#' @param title.size An \code{integer} that indicates the size of title.
+#' @param axis.label.size An \code{integer} that indicates the size of label.
+#' @param axis.tick.size An \code{integer} that indicates the size of tick mark.
+#' @param axis.x.label A \code{string} that indicates the label on x axis.
+#' @param axis.y.label A \code{string} that indicates the label on y axis.
+#' @param ... other arguments passed to specific methods
+#' @return A ggplot2 panel containing the graph of time series.
+#' @author Wenchao
+#' @examples
+#' x = gen.gts(WN(sigma2=1), 50)
+#' autoplot(x)
+autoplot.gts = function(object, to.unit = NULL, background = 'white',  
+                    line.type = 'solid', line.color = '#003C7D',
+                    point.size = 0, point.shape = 20,
+                    title = NULL, title.size= 15, 
+                    axis.label.size = 13, axis.tick.size = 11, 
+                    axis.x.label = NULL, axis.y.label = NULL, ... ){
   x1=y1=NULL
   
   if( !(background %in% c('grey','gray', 'white')) ){
@@ -84,25 +228,25 @@ plot.gts = function(x, background = 'white',
     background = 'white'
   }
   
-  if(class(x)!='gts'){stop('x must be a gts object. Use function gts() to create it.')}
-  if(x$freq == 0){stop('freq cannot be zero.')}
+  if(!is(object,"gts")){stop('object must be a gts object. Use function gts() or gen.gts() to create it.')}
   
   #prepare data
-  len = length(x$data)
-  df = data.frame(y1 = x$data, x1 = 0:(len-1) )
+  #len = length(x$data)
+  #df = data.frame(y1 = x$data, x1 = 0:(len-1) )
+  df = data.frame(y1 = object$data, x1 = object$x )
   
-  if( is.null(x$unit) && is.null(to.unit)==F ){warning('Unit of x is NULL. Conversion cannot be done.')}
-  if( is.null(x$unit) == F ){
+  if( is.null(object$unit) && is.null(to.unit)==F ){warning('Unit of object is NULL. Conversion was not done.')}
+  if( is.null(object$unit) == F ){
     if( is.null(to.unit) == F){
-      obj = convert.gts(df$x1, x$unit, to.unit)
+      obj = unitConversion(df$x1, object$unit, to.unit)
       if(obj$converted){
         df$x1 = obj$x
-        axis.x.label = paste0(axis.x.label, ' (',to.unit,')')
+        message(paste0('Unit of object is converted from ', object$unit, ' to ', to.unit), appendLF = T)
       }
     }
   }
   #deal with freq
-  df$x1 = df$x1/x$freq
+  df$x1 = df$x1/object$freq
   
   p = ggplot(data = df, mapping = aes(x = x1,y = y1)) + geom_line(linetype = line.type, color = line.color) + 
     geom_point(color = line.color, size = point.size, shape = point.shape)
@@ -128,49 +272,60 @@ plot.gts = function(x, background = 'white',
 # }
 
 #' @title Convert Unit of Time Series Data
-#' @param x A \code{vector}.
+#' @param x A \code{vector} containing the values on x-axis.
 #' @param from.unit A \code{string} indicating the unit which the data is converted from.
 #' @param to.unit A \code{string} indicating the unit which the data is converted to.
-#' @return A list with the following structure:
+#' @details
+#' The supported units are "ns"(nanosecond), "ms"(millisecond), "sec", "min", "hour", "day", "month", and "year".
+#' Make sure \code{from.unit} and \code{to.unit} are not \code{NULL} before it is passed to this function.
+#' @return A \code{list} with the following structure:
 #' \itemize{
-#'  \item{x} {Data.}
-#'  \item{converted} {A \code{boolean} indicating whether conversion is made.}
+#'  \item{x:} {Data.}
+#'  \item{converted:} {A \code{boolean} indicating whether conversion is made.}
 #' }
 #' @examples
 #' x = seq(60, 3600, 60)
-#' convert.gts(x, 'sec', 'min')
+#' unitConversion(x, 'sec', 'min')
 #' y = 1:10
-#' convert.gts(y, 'hour', 'sec')
-convert.gts = function(x, from.unit, to.unit){
-  #second, min, hour, day, month, year
-  unit = c(se = 1, mi = 2, ho = 3, da = 4, mo = 5, ye = 6)
+#' unitConversion(y, 'hour', 'sec')
+unitConversion = function(x, from.unit, to.unit){
+  #ns, ms, second, min, hour, day, month, year
+  unit = c(ns = 1, ms = 2,se = 3, mi = 4, ho = 5, da = 6, mo = 7, ye = 8)
   
   #assume 1 month = 30 days
-  ratio = c(60, 60, 24, 30, 12)
-  from.unit_1 = substr(from.unit, 1, 2)
-  to.unit_1 = substr(to.unit, 1, 2)
+  ratio = c(1E6, 1E3, 60, 60, 24, 30, 12)
+  from.unit.1 = substr(from.unit, 1, 2)
+  to.unit.1 = substr(to.unit, 1, 2)
   
   #check unit:
-  no_convert = F
-  if(from.unit_1 == to.unit_1){no_convert = T}
-  if(is.na(unit[from.unit_1]) ) {
-    message = paste('No such unit: ', from.unit, '. Available units are sec, min, hour, day, month and year. Conversion is terminated.', sep = '')
-    warning(message); no_convert = T}
-  if(is.na(unit[to.unit_1]) ) {
-    message = paste('No such unit: ', to.unit, '. Available units are sec, min, hour, day, month and year. Conversion is terminated.', sep = '')
-    warning(message); no_convert = T}
+  no.convert = F
+  if(from.unit.1 == to.unit.1){no.convert = T}
+  if(is.na(unit[from.unit.1]) ) {
+    message = paste('No such unit: ', from.unit, '. Supported units are "ns"(nanosecond), "ms"(millisecond), "sec", "min", "hour", "day", "month", and "year". Conversion is terminated.', sep = '')
+    warning(message); no.convert = T}
+  if(is.na(unit[to.unit.1]) ) {
+    message = paste('No such unit: ', to.unit, '. Supported units are "ns"(nanosecond), "ms"(millisecond), "sec", "min", "hour", "day", "month", and "year". Conversion is terminated.', sep = '')
+    warning(message); no.convert = T}
   
-  if(!no_convert){
-    if(unit[from.unit_1] > unit[to.unit_1]){
-      temp = ratio[unit[to.unit_1]: (unit[from.unit_1]-1)]
+  if(!no.convert){
+    #print out warning when day is convert to month, or month is converted to day.
+    conversionRange = unit[from.unit.1] : unit[to.unit.1]
+    if(6 %in% conversionRange && 7 %in% conversionRange){
+      warning('Unit conversion might be wrong because this function simply assumes 1 month = 30 days.')
+    }
+  }
+  
+  if(!no.convert){
+    if(unit[from.unit.1] > unit[to.unit.1]){
+      temp = ratio[unit[to.unit.1]: (unit[from.unit.1]-1)]
       multiplier = prod(temp)
       x = x*multiplier
     }else{
-      temp = ratio[unit[from.unit_1]: (unit[to.unit_1]-1) ]
+      temp = ratio[unit[from.unit.1]: (unit[to.unit.1]-1) ]
       multiplier = prod(temp)
       x = x/multiplier
     }
   }
-  obj = list(x = x, converted = !no_convert)  
+  obj = list(x = x, converted = !no.convert)  
   return(obj)
 }
