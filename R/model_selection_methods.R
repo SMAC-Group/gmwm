5a3f84ba13894189974b75205d15d5cf26a56dee
James J Balamuta <james.balamuta@gmail.com>
2015-09-10 21:46:16 -0500

    Improved automatic modeling logic!
    Former-commit-id: 34262483637841bca0afb9fdfcce3953f09de189

--- R/model_selection_methods.R
+++ R/model_selection_methods.R
@@ -27,10 +27,28 @@ select.desc.check = function(desc){
   
 }
 
+#' @title Formats the model score matrix
+#' @description The model score matrix receives the appropriate model numbering, col descriptors, and ordering of models.
+#' @param out A \code{list} containing the model matrix
+#' @param num.models A \code{integer} that indicates the number of models fit. 
+#' @return An updated matrix in the ith position of the list. 
+pretty.model.score = function(out, model.names){
+
+  colnames(out) = c("Obj Fun", "Optimism", "Criterion", "GoF P-Value")
+  rownames(out) = sapply(model.names, FUN = paste0, collapse=" ")
+  
+  out = out[order(out[,3]), ]
+  
+  rownames(out) = paste0(1:nrow(out), ". ", rownames(out) )
+  
+  out
+}
+
+
 #' @title Automatically select appropriate model for a set of models
 #' @description Runs through a model selection algorithm to determine the best model in a given set
 #' @param ... Multiple \code{ts.model} objects
-#' @param data A \code{vector}, \code{matrix}, \code{data.frame}, or \code{imu} object with either 1, 3, or 6 columns. 
+#' @param data A \code{vector}, \code{data.frame}, \code{matrix}, or \code{gts} object with 1 column.
 #' @param bootstrap A \code{bool} that is either true or false to indicate whether we use bootstrap or asymptotic By default, we use asymptotic.
 #' @param alpha A \code{double} that indicates the level of confidence for the WV CI.
 #' @param robust A \code{boolean} that indicates whether to use robust estimation.
@@ -39,17 +57,14 @@ select.desc.check = function(desc){
 #' @param G A \code{integer} that indicates the amount of guesses for caliberating the startup.
 #' @details The models MUST be nested within each other. If the models are not nested, the algorithm creates the "common denominator" model.
 #' @return A \code{rank.models} object.
-rank.models = function(..., data, bootstrap = F, model.type="ssm", alpha = 0.05, robust = F, eff = 0.6, B = 20, G = 1000, seed = 1337){
+rank.models = function(data, models=list(AR1()+WN(), AR1()), nested = F, bootstrap = F, model.type="ssm", alpha = 0.05, robust = F, eff = 0.6, B = 50, G = 10000, seed = 1337){
   
   set.seed(seed)
-  
-  obj_list = list(...)
-  numObj = length(obj_list)
-  
+  numObj = length(models)
   desc = vector("list", numObj) 
   
   for(i in 1:numObj){
-    mod = obj_list[[i]]
+    mod = models[[i]]
     
     if(!is(mod, 'ts.model')){
       stop("Ill-formed ... request. Detected non-ts.model object in ... Please specify parameters with names")
@@ -59,19 +74,34 @@ rank.models = function(..., data, bootstrap = F, model.type="ssm", alpha = 0.05,
     desc[[i]] = mod$desc
   }
   
-  out = .Call('GMWM_find_full_model', PACKAGE = 'GMWM', x = desc)
-
-  if(!any(sapply(desc, function(x, want) isTRUE(all.equal(x, want)),  out)) ){
-    print("Creating a Common Denominator Model!")
-    desc[[length(desc)+1]] = out
+  
+  if(nested == F){
+    full.str = .Call('GMWM_find_full_model', PACKAGE = 'GMWM', x = desc)
+    
+    if(!any(sapply(desc, function(x, want) isTRUE(all.equal(x, want)),  full.str)) ){
+      print("Creating a Common Denominator Model!")
+      desc[[length(desc)+1]] = full.str
+    }
+    desc = vector_to_set(desc)
+  }else{
+    
+    full.str = models[[1]]$desc
+    
+    m = as.matrix(comb.mat(length(full.str)))
+    m = m[-nrow(m),]
+    
+    desc = build_model_set(m, full.str)
+    
   }
   
-  out = .Call('GMWM_rank_models', PACKAGE = 'GMWM', data, model_str=desc, full_model=out, alpha, compute_v = "fast", model_type = model.type, K=1, H=B, G, robust, eff, bootstrap)
+  out = .Call('GMWM_rank_models', PACKAGE = 'GMWM', data, model_str=desc, full_model=full.str, alpha, compute_v = "fast", model_type = model.type, K=1, H=B, G, robust, eff, bootstrap)
   
-  return(out)
+  out[[1]][[1]][[1]] = pretty.model.score(out[[1]][[1]][[1]], desc)
+  
+  class(out) = c("rank.models")
+  
+  out
 }
-
-#nested.models 
 
 #' @title Automatically select appropriate model for IMU
 #' @description Runs through a model selection algorithm to determine the best model
@@ -96,12 +126,35 @@ auto.imu = function(data, model = 3*AR1()+WN()+RW()+QN()+DR(), bootstrap = F, al
   out = .Call('GMWM_auto_imu', PACKAGE = 'GMWM', data, combs=m, full_model=full.str, alpha, compute_v = "fast", model_type = "sensor", K=1, H=B, G, robust, eff, bootstrap)
   model.names = build_model_set(m, full.str) 
   
-  colnames(out[[1]][[1]][[1]]) = c("Obj Fun", "Optimism", "Criterion", "GoF P-Value")
-  rownames(out[[1]][[1]][[1]]) = sapply(model.names, FUN = paste0, collapse=" ")
+  for(i in 1:ncol(data)){
+    out[[i]][[1]][[1]] = pretty.model.score(out[[i]][[1]][[1]], model.names)
+  }
   
-  m = out[[1]][[1]][[1]]
+  class(out) = c("auto.imu","rank.models")
+  out  
+}
+
+print.rank.models = function(object, ...){
+  summary.rank.models(object)
+}
+
+print.auto.imu = function(object, ...){
+  summary.auto.imu(object)
+}
+
+summary.auto.imu = function(object, ...){
   
-  out[[1]][[1]][[1]] = m[order(m[,3]), ]
+  n.process = length(object)
   
-  return(out)
+  cat(paste0("There were ", n.process, " observed\n\n"))
+  
+  for(i in 1:n.process){
+    cat(paste0("The model ranking for data column ", i, ": \n"))
+    print(object[[i]][[1]][[1]])
+  }
+}
+
+summary.rank.models = function(object, ...){
+  cat("The model ranking is given as: \n")
+  print(object[[1]][[1]][[1]])  
 }
