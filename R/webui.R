7b46227ad1b82e8658301ddd1c537ec77e1f3eec
coatless <balamut2@illinois.edu>
2016-01-19 13:01:26 -0600

    Initial shiny build.

---
+++ R/webui.R
@@ -0,0 +1,21 @@
+#' @title Launch Web Modeling UI
+#' @description
+#' Creates a Web UI Modeling Environment to work with the GMWM objects
+#' @examples 
+#' \dontrun{
+#' # Launch the modeling UI
+#' webui()
+#' }
+webui = function(){
+  appDir = system.file("webui", package = "gmwm")
+  if(!nzchar(appDir)){
+    stop("Could not find the webui directory. Try re-installing `gmwm`.", call. = FALSE)
+  }
+  
+  if (requireNamespace("shiny", quietly = TRUE)) {
+    message('Hit <escape> in console to stop the application.')
+    shiny::runApp(appDir, display.mode = "normal")
+  } else {
+    stop("In order to use the `webui`, please install the `shiny` r package via: `install.packages('shiny')`")
+  }
+}
