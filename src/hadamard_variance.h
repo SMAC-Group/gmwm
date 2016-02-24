68c88e340b4333fd8be66d1860bd2e9ea11da3c6
avinash2692 <avinash_26_92@hotmail.com>
2016-02-23 22:08:04 -0600

    Header file for Hadam

---
+++ src/hadamard_variance.h
@@ -0,0 +1,25 @@
+/* Copyright (C) 2014 - 2015  James Balamuta, Stephane Guerrier, Roberto Molinari
+ *
+ * This file is part of GMWM R Methods Package
+ *
+ * The `gmwm` R package is free software: you can redistribute it and/or modify it
+ * under the terms of the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+ * as the LICENSE file.
+ *
+ * The `gmwm` R package is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the Attribution-NonCommercial-ShareAlike 4.0 International 
+ * (CC BY-NC-SA 4.0) along with `gmwm`.  If not, see <http://www.smac-group.com/licensing/>.
+ * 
+ */
+
+#ifndef HADAMARD_VARIANCE
+#define HADAMARD_VARIANCE
+
+arma::mat hadam_to_cpp(arma::vec x);
+
+arma::mat hadam_mo_cpp(arma::vec x);
+
+#endif
