aef8f8ad36f4e76f96aa9c0233866fba9f8c2e91
James J Balamuta <james.balamuta@gmail.com>
2016-05-16 16:10:52 -0500

    Feature: Generalized install package method
    Closes #178

--- R/data_install.R
+++ R/data_install.R
@@ -19,7 +19,7 @@
 #' @param loc  A \code{string} that contains the file location.
 #' @details 
 #' The IMU Data Package contains data from real life IMUs and is approximately a
-#'  13.8 MB download.
+#'  20 MB download.
 #' @author JJB
 #' @examples 
 #' \dontrun{
@@ -30,16 +30,53 @@
 #' install_imudata("LOCAL","C:/Users/James/Documents/imudata_x.y.z.tar.gz")
 #' }
 install_imudata = function(type="ONL",loc=NULL){
+  install_data_package("imudata", type, loc)
+}
+
+#' @title Install Data Package
+#' @description Downloads and Install the Data Package to use with the GMWM Package
+#' @param type A \code{string} with value \code{"ONL"} or \code{"LOCAL"}
+#' @param loc  A \code{string} that contains the file location.
+#' @author JJB
+#' @examples 
+#' \dontrun{
+#' # Online install
+#' install_datapkg()
+#' 
+#' # Local install
+#' install_datapkg("LOCAL","C:/Users/James/Documents/datapkg_x.y.z.tar.gz")
+#' }
+install_imudata = function(type="ONL",loc=NULL){
+  install_data_package("datapkg", type, loc)
+}
+
+
+#' @title Install a SMAC Group Data Package
+#' @description Downloads and Install the a SMAC Group Data Package to use.
+#' @param pkg  A \code{string} with the package name to install.
+#' @param type A \code{string} with value \code{"ONL"} or \code{"LOCAL"}
+#' @param loc  A \code{string} that contains the file location.
+#' @details 
+#' For a list of data packages please see the SMAC-Group data repo \url{http://smac-group.com/datarepo}.
+#' @author JJB
+#' @keywords internal
+#' @examples 
+#' \dontrun{
+#' # Online install
+#' install_data_package("imudata")
+#' 
+#' # Local install
+#' install_data_package("LOCAL","C:/Users/James/Documents/imudata_x.y.z.tar.gz")
+#' }
+install_data_package = function(pkg = "imudata", type = "ONL", loc = NULL){
   type = toupper(type)
   
   if(type == "ONL"){
-    install.packages("imudata", repos = "http://smac-group.com/datarepo")
+    install.packages(pkg, repos = "http://smac-group.com/datarepo")
   }else{
     if(!is.null(loc)){ 
       message("Installing the package using a local .tar file.")
       message("Note: You must have a compiler installed!")
-      message("The following packages are also required:")
-      message("ggplot2 and RcppArmadillo")
       install.packages(loc, type="source")
     }else{ 
       message("To install locally, you must also set the `loc` paramter!")
@@ -47,3 +84,4 @@ install_imudata = function(type="ONL",loc=NULL){
   }
   
 }
+
