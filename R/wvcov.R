83fc27883cf47872c0f072d64d01bb2749cb6794
James Joseph Balamuta <balamut2@illinois.edu>
2016-10-20 14:53:05 -0500

    License Change (CC BY-NC-SA 4.0 => AGPL v3.0), Copyright 2016

--- R/wvcov.R
+++ R/wvcov.R
@@ -1,17 +1,18 @@
-# Copyright (C) 2014 - 2015  James Balamuta, Stephane Guerrier, Roberto Molinari
+# Copyright (C) 2014 - 2016  James Balamuta, Stephane Guerrier, Roberto Molinari
 #
 # This file is part of GMWM R Methods Package
 #
-# The `gmwm` R package is free software: you can redistribute it and/or modify it
-# under the terms of the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
-# included within the packages source as the LICENSE file.
+# The `gmwm` R package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
 # The `gmwm` R package is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
-# You should have received a copy of the Attribution-NonCommercial-ShareAlike 4.0 International 
-# (CC BY-NC-SA 4.0) along with `gmwm`.  If not, see <http://www.smac-group.com/licensing/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #' @title Calculate the Asymptotic Covariance Matrix
 #' @description Places the Asymptotic Covariance Matrix in print form.
